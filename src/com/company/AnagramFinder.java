a09f5da90e5916067dd4aae70d93fbcb4edb5439
susheelkbhargav <susheelkbhargav@gmail.com>
2018-03-24 17:32:31 -0700

    Evaluating runtimes for all techniques for case2 according to the project document.
    Evaluating 3 techniques to find anagrams in a dictionary.
    
    Apparently the sum technique doesn't work

---
+++ src/com/company/AnagramFinder.java
@@ -0,0 +1,190 @@
+package com.company;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramFinder {
+    
+    public static void main(String[] args) throws FileNotFoundException,IOException {
+    	welcomeMessage();
+    	ArrayList <String> wordList= new ArrayList<String>();
+    	wordList=loadFile(args[0]);
+    	
+    	
+        
+        while (true) {
+        	System.out.println();
+        	System.out.println();
+            System.out.print("AnagramFinder> ");
+            Scanner scanner2= new Scanner(System.in);
+        	String inputWord = scanner2.nextLine();
+        	 if ("exit".equals(inputWord)) {
+        		scanner2.close();
+             	System.exit(0); 
+             }
+        	 else {
+        		 FindAnagramUsingSort(inputWord,wordList);
+                 FindAnagramUsingSum(inputWord,wordList);
+                 FindAnagramUsingCharacterCount(inputWord,wordList);
+
+        	 } 
+           
+        }
+    }
+    
+    private static ArrayList<String> loadFile(String args) throws FileNotFoundException {
+    	long startTime= System.currentTimeMillis();
+    	Scanner scanner = new Scanner(new File(args));
+    	ArrayList<String> wordList = new ArrayList<String>();
+    	while((scanner.hasNextLine())){
+            wordList.add(scanner.nextLine());
+            }
+        
+        scanner.close();
+        long stopTime= System.currentTimeMillis();
+
+        long endTime= stopTime-startTime;
+        System.out.println("Dictionary Loaded in "+ endTime+"" +"milli seconds");
+        
+       return wordList;
+    }
+    private static void welcomeMessage() {
+    	System.out.println("Welcome To Anagram Finder");
+    	System.out.println();
+    	System.out.println("-----------------------------------------------------------------");
+    	
+    }
+    
+    private static void FindAnagramUsingSort(String inputWord, ArrayList<String> list) {
+    	System.out.println("============");
+        System.out.println("Using Sort");
+        long startTime= System.currentTimeMillis();
+        for(int i=0; i<list.size();i++){
+            String word= list.get(i);
+            if(isAnagramUsingSort(word,inputWord)){
+                System.out.println("Found anagram"+" "+ word);
+
+            }
+        }
+        long stopTime= System.currentTimeMillis();
+
+        long endTime= stopTime-startTime;
+        System.out.println("Time to find all anagrams is "+ endTime+"" +"milli seconds");
+        
+    }
+    private static void FindAnagramUsingSum(String inputWord, ArrayList<String> list) {
+    	System.out.println("============");
+        System.out.println("Using Sum");
+        long startTime= System.currentTimeMillis();
+        for(int i=0; i<list.size();i++){
+            String word= list.get(i);
+                if(isAnagramUsingSum(word,inputWord)){
+                    System.out.println("Found anagram"+" "+ word);
+
+                }
+            }
+        
+        long stopTime= System.currentTimeMillis();
+        long endTime= stopTime-startTime;
+        System.out.println("Time to find all anagrams is "+ endTime+"" +"milli seconds");
+        
+
+    }
+
+    private static void FindAnagramUsingCharacterCount(String inputWord, ArrayList<String> list) {
+    	System.out.println("============");
+        System.out.println("Using character count");
+        long startTime= System.currentTimeMillis();
+        
+        for(int i=0; i<list.size();i++){
+           
+        	String word= list.get(i);
+           
+            if(isAnagramUsingCharacterCount(word,inputWord)){
+                
+            	System.out.println("Found anagram"+" "+ word);
+
+            }
+        }
+        long stopTime= System.currentTimeMillis();
+
+        long endTime= stopTime-startTime;
+        
+        System.out.println("Time to find all anagrams is "+ endTime+"" +"milli seconds");
+        
+    }
+
+    static boolean isAnagramUsingSort(String s1, String s2)
+    {
+        //Initially setting status as true
+
+        boolean status = true;
+        //checking the length
+        if(s1.length() != s2.length())
+        {
+            status = false;
+        }
+        else
+        {
+            //Changing the case of characters of both cs1 and cs2 and converting them to char array
+
+            char[] arrayS1 = s1.replaceAll("\\s", "").toLowerCase().toCharArray();
+
+            char[] arrayS2 = s2.replaceAll("\\s", "").toLowerCase().toCharArray();
+
+            //Sorting both arrayS1 and arrayS2
+
+            Arrays.sort(arrayS1);
+
+            Arrays.sort(arrayS2);
+
+            //Checking whether arrayS1 and arrayS2 are equal
+
+            status = Arrays.equals(arrayS1, arrayS2);
+        }
+
+        //Output
+        return status;
+    }
+
+    static boolean isAnagramUsingCharacterCount(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        int[] letters= new int[128];
+        String modifiedS1 = s1.replaceAll("\\s", "").toLowerCase();
+        String modifiedS2 = s2.replaceAll("\\s", "").toLowerCase();
+       // char[] arrayS2 = s2.replaceAll("\\s", "").toLowerCase().toCharArray();
+
+        for (char c:modifiedS1.toCharArray()){
+            //count each number of char in s
+            letters[c]++;
+        }
+        for(int i=0;i<modifiedS2.length();i++){
+            int c= (int)modifiedS2.charAt(i);
+            if(letters[c]!=1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isAnagramUsingSum(String s1, String s2) {
+        String modifiedS1 = s1.replaceAll("\\s", "").toLowerCase();
+        String modifiedS2 = s2.replaceAll("\\s", "").toLowerCase();
+        long sum1 = 0;
+        long sum2 = 0;
+
+        for (int i = 0; i < modifiedS1.length(); i++) {
+            int c = (int) modifiedS1.charAt(i);
+            sum1 += c;
+        }
+
+        for (int i = 0; i < modifiedS2.length(); i++) {
+            int c = (int) modifiedS2.charAt(i);
+            sum2 += c;
+        }
+        return sum1 == sum2;
+    }
+}
